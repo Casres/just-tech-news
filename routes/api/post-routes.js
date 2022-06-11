4aa9a35ae5e5100b76be5451af7f4171aa147914
Casres <christian@casillas.tech>
2022-06-10 22:10:32 -0400

    Testing pull from develop branch to feature/post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { Post, User } = require("../../models");
-const { post } = require("./user-routes");
+
 // gets all posts
 router.get("/", (req, res) => {
   console.log("==============================");
   Post.findAll({
     attributes: ["id", "post_url", "title", "created_at"],
-    order: [['created_at', 'DESC']],
+    order: [["created_at", "DESC"]],
     include: [
       {
         model: User,
