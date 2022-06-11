9d89699c15d8c7cfa233ee1980955dbd031af908
Casres <christian@casillas.tech>
2022-06-10 20:59:47 -0400

    lesson 13.3.6

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 
 // this gets all of the user routes and puts them into a variable name 'userRoutes
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 // this is using the file that has all of the user routes by accessing the file through the variable and putting '/users' in front of every single one of those routs
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
