9066c8b7f82b23c55a5f7bfa7effa9651b598bc3
Casres <christian@casillas.tech>
2022-06-25 01:46:11 -0400

    SP

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 // for the css
-const path = require('path');
+const path = require("path");
 
-// for the handlebars 
-const exphbs = require('express-handlebars');
+// for the handlebars
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 // for the endpoints
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
+const express = require("express");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
 
 // servers
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// front-end HTML
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// makes a session (a cookie) from the server (express)
+const session = require("express-session");
+// 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: "Super Secret Secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// front-end HTML
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // for the css
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -29,5 +45,7 @@ app.use(routes);
 
 // turns on connection to DB and Server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`SERVER ONLINE, now listening to port ${PORT}` ));
-}); 
+  app.listen(PORT, () =>
+    console.log(`SERVER ONLINE, now listening to port ${PORT}`)
+  );
+});
